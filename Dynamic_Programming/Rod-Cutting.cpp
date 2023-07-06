9f05f8afe12724e75e8b9f028a1d11e0bbe9f9fb
yogeshdewangan07 <yogeshrameshdewangan@gmail.com>
2023-07-06 15:24:12 +0530

    Rod Cutting Solution

---
+++ Dynamic_Programming/Rod-Cutting.cpp
@@ -0,0 +1,28 @@
+// User function Template for C++
+
+class Solution{
+  public:
+    
+    int cutRod(int price[], int n) {
+        vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        
+        for(int N = 0; N <= n; N++){
+            dp[0][N] = N * price[0];
+        }
+        
+        for(int ind = 1; ind < n; ind++){
+            for(int N = 0; N <= n; N++){
+                int notTake = 0 + dp[ind-1][N];
+                int take = INT_MIN;
+                int rodLength = ind + 1;
+                if(rodLength <= N){
+                    take = price[ind] + dp[ind][N-rodLength];
+                }
+                
+                dp[ind][N] = max(take, notTake);
+            }
+        }
+        
+        return dp[n-1][n];
+    }
+};
